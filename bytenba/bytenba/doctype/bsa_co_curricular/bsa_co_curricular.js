309c62bb9d10d500f88d44525bc1eb0e33f7363a
vinay1133 <vinaybhojwani75@gmail.com>
2023-09-10 19:11:09 +0000

    AI-3.3 modified and AI-3.4 added

--- bytenba/bytenba/doctype/bsa_co_curricular/bsa_co_curricular.js
+++ bytenba/bytenba/doctype/bsa_co_curricular/bsa_co_curricular.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, byte_team and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("BSA-co-cirricular", {
+// frappe.ui.form.on("BSA-Co-curricular", {
 // 	refresh(frm) {
 
 // 	},
