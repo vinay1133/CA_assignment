63bd7d90379d393585ac8726d7618b1d5ac66e01
Ishaanapte0108 <ishaanmapte@gmail.com>
2023-09-23 16:01:45 +0000

    new changes

--- bytenba/public/js/custom_cert_courses.js
+++ bytenba/public/js/custom_cert_courses.js
@@ -18,3 +18,13 @@ frappe.ui.form.on("Courses taught", {
 		frm.refresh_field("reviewer");
 	}
 });
+frappe.ui.form.on("BSA industrial visit", {
+	refresh(frm) {
+		frm.set_query('reviewer', function() {
+			return {
+				query: "bytenba.get_professors.get_professor_names"
+			};
+		});
+		frm.refresh_field("reviewer");
+	}
+});
