d6cbfda579566a830f8c4e5c345ee3ed7e3ee084
Ishaanapte0108 <ishaanmapte@gmail.com>
2023-10-23 21:36:30 +0530

    newupdates

--- bytenba/bytenba/doctype/lab_work_case_study/test_lab_work_case_study.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, byte_team and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestLabworkCasestudy(FrappeTestCase):
-	pass
--- bytenba/bytenba/doctype/laboratory_work_or_case_studies/laboratory_work_or_case_studies.py
+++ bytenba/bytenba/doctype/laboratory_work_or_case_studies/laboratory_work_or_case_studies.py
@@ -4,6 +4,8 @@
 import frappe
 from frappe.model.document import Document
 import bytenba.custom_utilities as uf
+import re
+
 pattern_for_wtg = r'\((\s*(?:\d+\.\d+|\d+)\s*)\)'
 
 Doctype = "Laboratory Work Or Case Studies"
@@ -22,8 +24,72 @@ class LaboratoryWorkOrCaseStudies(Document):
 		uf.validateAY(self.academic_year)
 		existing_record = frappe.db.exists(Doctype, {'name': self.name})
 		if existing_record and existing_record != self.name:
-			frappe.throw('There already exists such a record in the database')  
+			frappe.throw('There already exists such a record in the database')
 
 
 def compute_marks(self):
-	pass
+	
+	for item in self.lab_work_case_study_ct:
+		return item.mms1
+	# count = 0
+	# appraisal_score = 0
+	
+	# for item in self.lab_work_case_study_ct:
+
+	# 	count += 1
+
+	# 	mms_category = False
+	# 	engg_category = False
+
+	# 	mms1 = item.mms_a
+	# 	mms2 = item.mms_b
+		
+	# 	engg1 = item.engg_a
+	# 	engg2 = item.engg_b
+	# 	engg3 = item.engg_c
+
+	# 	match = re.search(pattern_for_wtg, self.marks_obtained)
+	# 	if match:
+	# 		mappingval = float(match.group(1).strip())
+	# 	else:
+	# 		frappe.throw('Error Fetching Field Weightages')
+		
+	# 	if mms1 and mms2:
+	# 		mms_category = True
+	# 		if mms1 < 0 or mms2 < 0:
+	# 			frappe.throw(f"MMS weights cannot be negative, error in row {count}")
+	# 		if (mms1 > 1.5 or mms2 > 1):
+	# 			frappe.throw(f"MMS weights cannot exceed specified ranges, error in row {count}")
+	# 		mms = mms1*mms2
+		
+	# 	if engg1 and engg2 and engg3:
+	# 		if engg1 < 0 or engg2 < 0 or engg3 < 0:
+	# 			frappe.throw(f"Engg weights cannot be negative, error in row {count}")
+	# 		if (engg1 > 1.5 or engg2 > 1.25 or engg3 > 1):
+	# 			frappe.throw(f"Engg weights cannot exceed specified ranges, error in row {count}")
+	# 		engg_category = True
+	# 		engg = engg1*engg2*engg3
+		
+	# 	if mms_category and engg_category:
+	# 		frappe.throw(f'Cannot fill both categories in the same row, error in row {count} ')
+	# 	if mms_category:
+	# 		item.marks_obtained = mms*mappingval
+	# 	if engg_category:
+	# 		item.marks_obtained = engg*mappingval
+	# 	else:
+	# 		frappe.throw(f'Values incorrectly entered in row {count}')
+		
+	# 	appraisal_score += item.marks_obtained
+	
+	# return appraisal_score / count
+
+
+
+
+
+
+		
+
+		
+
+					
