1ac4896f158aec40f3390122c2700c138dbfefa3
Ishaanapte0108 <ishaanmapte@gmail.com>
2023-11-06 23:42:35 +0530

    created Ab1

--- bytenba/administrative_bucket/doctype/institute_level_academic_activities_organized/institute_level_academic_activities_organized.py
+++ bytenba/administrative_bucket/doctype/institute_level_academic_activities_organized/institute_level_academic_activities_organized.py
@@ -3,49 +3,61 @@ from frappe.model.document import Document
 import bytenba.custom_utilities as uf
 import re
 
+Doctype = 'Institute Level Academic Activities Organized'
+pattern_for_wtg = r'\((\s*(?:\d+\.\d+|\d+)\s*)\)'
+
 class InstituteLevelAcademicActivitiesOrganized(Document):
 	
+	"""method to autoname your document"""
 	def autoname(self):
-		self.name = f'AI2_{self.professor}_{self.academic_year}_{self.semester}'
-
-	def before_save(self):
-		self.self_appraisal_score = compute_marks(self)
+		self.name = f'AB1_{self.professor}_{self.academic_year}_{self.semester}'
 	
+	def before_save(self):
+		self.self_appraisal_score = round(compute_marks(self))
+
 	def validate(self):
-		"""Validation for academic year"""
 		uf.validateAY(self.academic_year)
+		existing_record = frappe.db.exists(Doctype, {'name': self.name})
+		if existing_record and existing_record != self.name:
+			frappe.throw('There already exists such a record in the database')  
+
 
 def compute_marks(self):
-		# 	# no. of rows
-		# count=0 
-		# """Average Target Obtained"""
-		# target_obt=0
-		# for item in self.course__lab_name:
-		# 	target_obt += item.no_sessions/item.number_of_sessions_as_per_syllabus*100
-		# 	count += 1
-		# avg_target_obt = target_obt/count
-		# self.average_target_obtained = avg_target_obt
+	counter = 0
+	pas = []
 
-		# """Average Completion of Syllabus"""
-		# comp_of_syllabus=0
-		# for item in self.course__lab_name:
-		# 	comp_of_syllabus += item.completion_of_syllabus
-		# avg_comp_of_syllabus = comp_of_syllabus/count
-		# self.average_syllabus_completed = avg_comp_of_syllabus
+	for item in self.criteria_table:
+		if counter > 1:
+			frappe.throw('Can only have two entries in the table')
 
-		# """Marks Obtained"""
-		# if avg_target_obt>=100:
-		# 	self.marks_obtained = 300
-		# elif avg_target_obt>90 and avg_target_obt<=99:
-		# 	self.marks_obtained = 225
-		# elif avg_target_obt>80 and avg_target_obt<=89:
-		# 	self.marks_obtained = 150
-		# elif avg_target_obt>70 and avg_target_obt<=79:
-		# 	self.marks_obtained = 100
-		# else:
-		# 	self.marks_obtained = 0
-		
-		# """Self-Appraisal Score"""
-		# self.self_appraisal_score = self.marks_obtained*self.average_syllabus_completed/100
+		else:
+			counter +=1
 
-		return 10
+			match = re.search(pattern_for_wtg, item.quality_of_conduct_of_activity)
+			if match:
+				val1 = float(match.group(1).strip())
+			else:
+				frappe.throw('Error Fetching Field Weightages')
+			
+			match = re.search(pattern_for_wtg, item.associations_in_the_activity)
+			if match:
+				val2 = float(match.group(1).strip())
+			else:
+				frappe.throw('Error Fetching Field Weightages')			
+
+			match = re.search(pattern_for_wtg, item.paper_selection_ratio_after_review)
+			if match:
+				val3 = float(match.group(1).strip())
+			else:
+				frappe.throw('Error Fetching Field Weightages')
+
+			match = re.search(pattern_for_wtg, item.designation)
+			if match:
+				val4 = float(match.group(1).strip())
+			else:
+				frappe.throw('Error Fetching Field Weightages')			
+			
+			pow = val1*val2*val3*val4*100
+			pas.append(pow)
+	
+	return pas[0] + pas[1]
